440d7e3666017b993f34dbe346dc096176f837d1
ajose-malik <ajose.malik@gmail.com>
2021-02-20 11:00:28 -0500

    Created sign-in component

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -3,6 +3,7 @@ import { Switch, Route } from "react-router-dom";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
+import SigninAndSignup from "./pages/signin-signup/signin-signup.component";
 import "./App.css";
 
 const App = () => {
@@ -12,6 +13,7 @@ const App = () => {
 			<Switch>
 				<Route exact path="/" component={HomePage} />
 				<Route path="/shop" component={ShopPage} />
+				<Route path="/signin" component={SigninAndSignup} />
 			</Switch>
 		</div>
 	);
