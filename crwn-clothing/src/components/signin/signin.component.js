b6f505acbc6e6010497fb7a86c7033c613fe5b3d
ajose-malik <ajose.malik@gmail.com>
2021-02-20 13:25:18 -0500

    created colors-app and add palette

--- crwn-clothing/src/components/signin/signin.component.js
+++ crwn-clothing/src/components/signin/signin.component.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
-import "./signin.styles.scss";
 import FormInput from "../form-input/form-input.component";
 import CustomButton from "../custom-button/custom-button.component";
+import "./signin.styles.scss";
 
 class SignIn extends Component {
 	constructor(props) {
