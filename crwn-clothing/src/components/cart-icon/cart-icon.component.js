1b20c44540fcf9c6f5caa388de3d329e97bf0f50
ajose-malik <ajose.malik@gmail.com>
2021-02-21 13:33:17 -0500

    add cart icon component

--- crwn-clothing/src/components/cart-icon/cart-icon.component.js
+++ crwn-clothing/src/components/cart-icon/cart-icon.component.js
@@ -2,14 +2,13 @@ import React from "react";
 import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
 import "./cart-icon.styles.scss";
 
-const CustomButton = ({ children, isGoogleSignIn, ...otherProps }) => {
+const CartIcon = () => {
 	return (
-		<button
-			className={`${isGoogleSignIn ? "google-sign-in" : ""} custom-button`}
-			{...otherProps}>
-			{children}
-		</button>
+		<div className="cart-icon">
+			<ShoppingIcon className="shopping-icon" />
+			<span className="item-count">0</span>
+		</div>
 	);
 };
 
-export default CustomButton;
+export default CartIcon;
