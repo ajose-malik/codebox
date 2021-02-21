7f6ea34e1c25bc9c2d9f60f234f30e7126c6edf6
ajose-malik <ajose.malik@gmail.com>
2021-02-21 09:51:26 -0500

    complete firestore auth set-up and add sign-up component and styling

--- crwn-clothing/src/firebase/firebase.utils.js
+++ crwn-clothing/src/firebase/firebase.utils.js
@@ -11,6 +11,29 @@ const config = {
 	appId: "1:873201414808:web:5e7d14482ac41a15fdd6ed"
 };
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+	if (!userAuth) return;
+	const userRef = firestore.doc(`users/${userAuth.uid}`);
+	const snapShot = await userRef.get();
+
+	if (!snapShot.exists) {
+		const { displayName, email } = userAuth;
+		const createdAt = new Date().toLocaleDateString();
+
+		try {
+			await userRef.set({
+				displayName,
+				email,
+				createdAt,
+				...additionalData
+			});
+		} catch (err) {
+			console.log("error creating use", err.message);
+		}
+	}
+	return userRef;
+};
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
