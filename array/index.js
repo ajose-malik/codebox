5e33ede0e59c9b197faecccadce8d010df7168fa
ajose-malik <ajose.malik@gmail.com>
2021-04-19 12:45:11 -0400

    Create pop method

--- array/index.js
+++ array/index.js
@@ -14,9 +14,23 @@ class myArray {
 		this.data[this.length] = item
 		this.length++
 	}
+
+	// Implement pop method
+	pop() {
+		// Option 1
+		delete this.data[this.length - 1]
+		this.length--
+
+		// Option 2
+		// delete this.data[Object.keys(this.data).length - 1]
+		// this.length--
+	}
 }
 
 const newArray = new myArray()
 newArray.push("hello world")
-// console.log(myArray.length)
+newArray.push("hello Bee")
+console.log(newArray.length)
+newArray.pop()
+console.log(newArray.length)
 console.log(newArray.get(0))
