d07a5b57a57282c3d7f1cbec06e0600df3b1e563
ajose-malik <ajose.malik@gmail.com>
2021-04-19 16:26:02 -0400

    Create another solution for reverseString

--- algo/reverseString.js
+++ algo/reverseString.js
@@ -2,20 +2,28 @@
 
 const stringToConvert = "kilaM si eman ym iH"
 
+// // Solution #1
+// const reverseString = str => {
+// 	// Check for edge cases
+// 	if (!str || str.length < 2 || typeof str !== "string") {
+// 		console.log("bad input")
+// 	}
+
+// 	const result = []
+// 	// Loop through string backwards and push character at each index
+// 	for (let i = str.length - 1; i >= 0; i--) {
+// 		result.push(str[i])
+// 	}
+
+// 	// Join all characters
+// 	console.log(result.join(""))
+// }
+
+// Solution #2
 const reverseString = str => {
-	// Check for edge cases
-	if (!str || str.length < 2 || typeof str !== "string") {
-		console.log("bad input")
-	}
+	const result = str.split("").reverse().join("")
 
-	const result = []
-	// Loop through string backwards and push character at each index
-	for (let i = str.length - 1; i >= 0; i--) {
-		result.push(str[i])
-	}
-
-	// Join all characters
-	console.log(result.join(""))
+	console.log(result)
 }
 
 reverseString(stringToConvert)
