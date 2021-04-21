d9e4f78d534ef04b47b2b0fa52dd0dcafdfa8de5
ajose-malik <ajose.malik@gmail.com>
2021-04-20 21:43:56 -0400

    Added router to events slug

--- events-project/frontend/pages/events/[slug].js
+++ events-project/frontend/pages/events/[slug].js
@@ -1,3 +1,14 @@
+import { useRouter } from "next/router"
+
 export default function EventPage() {
-	return <h1>Slugging</h1>
+	const router = useRouter()
+
+	console.log(router)
+	return (
+		<>
+			<h1>Slugging</h1>
+			<h3>{router.query.slug}</h3>
+			<button onClick={() => router.push("/")}>Home</button>
+		</>
+	)
 }
