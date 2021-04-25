33459b1a415f2ecc491269e62edd02c53b1deebd
ajose-malik <ajose.malik@gmail.com>
2021-04-25 10:53:57 -0400

    Refactored passwordValidator

--- algo/passwordValidator.js
+++ algo/passwordValidator.js
@@ -12,37 +12,56 @@
 
 const isValidPassword = (password, username) => {
 	// Check if password length
-	// if (password.length < 8) {
-	// 	console.log(false)
-	// 	return false
-	// }
-
-	// Check if password contains space
-	// if (password.indexOf(" ") !== -1) {
-	// 	console.log(false)
-	// 	return false
-	// }
-
-	// Check if password contains username
-	// if (password.includes(username)) {
-	// 	console.log(false)
-	// 	return false
-	// }
-
-	// REFACTORED
-	if (
-		password.length < 8 ||
-		password.indexOf(" ") !== -1 ||
-		password.includes(username)
-	) {
+	if (password.length < 8) {
 		console.log(false)
 		return false
 	}
 
+	// Check if password contains space
+	if (password.indexOf(" ") !== -1) {
+		console.log(false)
+		return false
+	}
+
+	// Check if password contains username
+	if (password.includes(username)) {
+		console.log(false)
+		return false
+	}
 	// If above if statements are false then return true
 	console.log(true)
 	return true
 }
+
+// // REFACTORED #1
+// const isValidPassword = (password, username) => {
+// 	if (
+// 		password.length < 8 ||
+// 		password.indexOf(" ") !== -1 ||
+// 		password.includes(username)
+// 	) {
+// 		console.log(false)
+// 		return false
+// 	}
+// 	// If above if statements are false then return true
+// 	console.log(true)
+// 	return true
+// }
+
+// // REFACTORED #2
+// const isValidPassword = (password, username) => {
+// 	const checkLength = password.length < 8
+// 	const checkSpace = password.indexOf(" ") !== -1
+// 	const checkSimilar = password.includes(username)
+// 	if (checkLength || checkSpace || checkSimilar) {
+// 		console.log(false)
+// 		return false
+// 	}
+
+// 	// If above if statements are false then return true
+// 	console.log(true)
+// 	return true
+// }
 
 isValidPassword("7hiwgidw9", "catfan") // true
 isValidPassword("catfan7382", "catfan") // false
