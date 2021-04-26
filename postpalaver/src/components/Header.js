2ab47dfc70417222ae71748a9024c5242d068504
ajose-malik <ajose.malik@gmail.com>
2021-04-26 15:19:38 -0400

    Active tab refresh fix

--- postpalaver/src/components/Header.js
+++ postpalaver/src/components/Header.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 import {
 	AppBar,
 	Toolbar,
@@ -21,14 +21,38 @@ export const Header = () => {
 		setValue(value)
 	}
 
+	// Check current URL and set value of tab indicator as needed
+	useEffect(() => {
+		if (window.location.pathname === "/" && value !== 0) {
+			setValue(0)
+		} else if (window.location.pathname === "/services" && value !== 1) {
+			setValue(1)
+		} else if (window.location.pathname === "/revolution" && value !== 2) {
+			setValue(2)
+		} else if (window.location.pathname === "/about" && value !== 3) {
+			setValue(3)
+		} else if (window.location.pathname === "/contact" && value !== 4) {
+			setValue(4)
+		} else {
+			setValue(5)
+		}
+	}, [])
+
 	return (
 		<>
 			<AppBar>
 				<Toolbar disableGutters>
-					<img src={Logo} alt="logo" className={logo} />
-					<Typography variant="h4" className={title} color="secondary">
-						Post Palaver
-					</Typography>
+					<Button
+						disableRipple
+						component={Link}
+						to="/"
+						onClick={() => setValue(0)}
+						style={{ padding: 0 }}>
+						<img src={Logo} alt="logo" className={logo} />
+						<Typography variant="h4" className={title} color="secondary">
+							Post Palaver
+						</Typography>
+					</Button>
 					<Tabs
 						value={value}
 						onChange={handleChange}
@@ -59,7 +83,9 @@ export const Header = () => {
 							className={tab}
 							size="small"
 							variant="outlined"
-							color="secondary">
+							color="secondary"
+							component={Link}
+							to="/estimate">
 							Estimate
 						</Button>
 					</Tabs>
