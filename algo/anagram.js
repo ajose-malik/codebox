0bd263d151e43cc0cf6a5f146c5e188e072dbd6c
ajose-malik <ajose.malik@gmail.com>
2021-04-26 09:20:18 -0400

    Solved anagram

---
+++ algo/anagram.js
@@ -0,0 +1,48 @@
+// Check if two strings are anagram. Expected output is true or false
+const string1 = "texttwisttime"
+const string2 = "timetwisttext"
+
+const validAnagram = (string1, string2) => {
+	// Convert strings to array
+	const array1 = string1.split("")
+	const array2 = string2.split("")
+
+	const counter1 = {}
+	const counter2 = {}
+
+	// Check if arguments are strings
+	if (typeof string1 !== "string" || typeof string2 !== "string") {
+		console.log("Wrong input")
+		return false
+	}
+
+	// Check if arguments are same length
+	if (array1.length !== array2.length) {
+		console.log(false)
+		return false
+	}
+
+	// Loop through Array1 and count char instances
+	for (let value of array1) {
+		counter1[value] = (counter1[value] || 0) + 1
+	}
+
+	// Loop through Array2 and count char instances
+	for (let value of array2) {
+		counter2[value] = (counter2[value] || 0) + 1
+	}
+
+	// Check if same char and same number of char exist in each counter
+	for (let key in counter1) {
+		// console.log(key)
+		if (counter1[key] !== counter2[key]) {
+			console.log(false)
+			return false
+		}
+	}
+
+	console.log(true)
+	return true
+}
+
+validAnagram(string1, string2)
